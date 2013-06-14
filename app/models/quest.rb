f96510ff2cfa7c8634690fe9c0a99fbc2133829a
hgsk <hgsk85@gmail.com>
2013-06-14 18:35:55 +0900

    fix quests view

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,6 +1,17 @@
+# Quest is similar to Board on BBS.
 class Quest < Resource
   include Mongoid::Document
 	field :title, type: String
+	field :description, type: String
 	field :expire_date, type: Time
 	field :content_url, type: String
+
+	def owner
+		self.user
+	end
+
+	def cooperators
+		# TODO get referenced users
+		[]
+	end
 end
